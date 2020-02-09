3de104b859f71d29d578a76f19b16d15e0ecd226
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-02-10 01:01:53 +0300

    fix order

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"Font Awesome 5 Free Solid:size=11", "Fira Code:size=11"};
+static const char *fonts[]          = {"Fira Code:size=11", "Font Awesome 5 Free Solid:size=11"};
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
